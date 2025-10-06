b78fcbe6cdd8f382bca2ffa7745c0966044cd12f
Alexandre <alexandredossantos878@hotmail.com>
2025-10-06 20:18:20 -0300

    Projeto 2.0: Versão Completamente nova com mais funcionalidades

---
+++ app.js
@@ -0,0 +1,61 @@
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+//let titulo = document.querySelector('h1');
+//titulo.innerHTML = 'Jogo do número secreto';
+
+//let paragrafo = document.querySelector('p');
+//paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+function exibirTextoNaTela(tag, texto){
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto
+}
+
+function exibirMensagemInicial(){
+  exibirTextoNaTela('h1', 'Jogo do número Secreto');
+  exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+  
+}
+
+
+
+function verificarChute() {
+  let chute = document.querySelector('input').value
+  
+  if(chute == numeroSecreto){
+    exibirTextoNaTela('h1', 'Você acertou!');
+    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa!';
+    let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`;
+    exibirTextoNaTela('p', mensagemTentativas);
+    document.getElementById('reiniciar').removeAttribute('disabled');
+  }else{
+    
+    if( chute > numeroSecreto){
+      exibirTextoNaTela('p', 'O número secreto é menor');
+    }else{
+      exibirTextoNaTela('p', 'O número secreto é maior');
+    }
+    tentativas++;
+    limparCampo();
+  }
+
+}
+
+function gerarNumeroAleatorio(){
+   return parseInt(Math.random() * 10 + 1);
+  
+}
+
+function limparCampo(){
+  chute = document.querySelector('input');
+  chute.value = '';
+}
+
+function reiniciarJogo(){
+  numeroSecreto = gerarNumeroAleatorio();
+  limparCampo();
+  tentativas = 1;
+  exibirMensagemInicial();
+  document.getElementById('reiniciar').setAttribute('disabled', 'true');
+
+}
